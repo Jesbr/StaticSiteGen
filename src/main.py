e66e90fdb9a831bab4015dc5c0911fafc6512a52
JesseB <jesse.anthony.b@gmail.com>
2025-10-12 23:31:38 -0500

    testing for publishing

--- src/main.py
+++ src/main.py
@@ -1,25 +1,29 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
-    print("Deleting public directory...")
+    base_path = sys.argv[1] if len(sys.argv) > 1 else "/"
+    print(f"Using base path: {base_path}")
+
+    print("Deleting docs directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
-    print("Copying static files to public directory...")
+    print("Copying static files to docs directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "docs", base_path)
 
 main()
